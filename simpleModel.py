530e5db9b9e464b28f7d4d1680d0c26dcd542c93
Ethan F <37308053+3zik@users.noreply.github.com>
2025-06-25 16:42:44 -0400

    hello world

---
+++ simpleModel.py
@@ -0,0 +1,16 @@
+import pybinding as pb
+from pybinding.repository import graphene
+
+
+print("Hello World")
+
+"""
+# Example: build twisted bilayer graphene
+model = pb.Model(
+    graphene.bilayer(),
+    pb.moire(angle=1.05),   # twist angle in degrees
+    pb.rectangle(50, 50),   # system size (nm)
+)
+model.plot()
+
+"""
